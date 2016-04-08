c070f00c02a79dd6e11633c816eab0b68609eeeb
Daeseok Youn <daeseok.youn@gmail.com>
2016-04-08 18:51:05 +0900

    [GoogleCodeJam] My simple template for c++

---
+++ Daeseok.Youn/GoogleCodeJam/template.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+    int testCase;
+    int casenum = 1;
+
+    cin >> testCase;
+
+    while (testCase-- > 0) {
+        // start implementation
+        // end
+
+        cout << "Case #" << casenum++ << ": ";
+        // result
+    }
+
+    return 0;
+}
