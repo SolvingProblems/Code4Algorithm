c14567366ba20c282336cfffd6632ffc84f844dc
Daeseok Youn <daeseok.youn@gmail.com>
2016-12-28 19:05:19 +0900

    Update teach.java
    change from array to bit operation.
    But it was failed with "timed out"

--- Daeseok.Youn/acmicpc/teach/teach.java
+++ Daeseok.Youn/acmicpc/teach/teach.java
@@ -5,43 +5,32 @@ import java.util.ArrayList;
 public class teach {
   public static int k;
 
-  public static int readable(List<String> words, boolean [] alpha)
-  {
-    int count = 0;
-
-    for (int i = 0; i < words.size(); i++) {
-      String str = words.get(i);
-      int j = 0;
-      for (j = 0; j < str.length(); j++) {
-        if (!alpha[str.charAt(j) - 'a'])
-          break;
-      }
-
-      if (j == str.length())
-        count++;
-    }
-
-    return count;
-  }
-
-  public static int countReadable(List<String> words, boolean [] alpha, char [] alpha2,
+  public static int countReadable(List<Integer> words, int alpha, char [] alpha2,
                                   int alpha2size, int idx)
   {
     int max = 0;
     int chCnt = 0;
 
     for (int j = 0; j < 26; j++) {
-      if (alpha[j])
+      if ((alpha & (1 << j)) != 0)
         chCnt++;
     }
 
-    if (chCnt == k)
-      return readable(words, alpha);
+    if (chCnt == k) {
+      chCnt = 0;
+      for (int i = 0; i < words.size(); i++) {
+        int val = words.get(i);
+        if ((val & alpha) == val)
+          chCnt++;
+      }
+
+      return chCnt;
+    }
 
     for (int i = idx; i < alpha2size; i++) {
-      alpha[alpha2[i] - 'a'] = true;
+      alpha |= (1 << (alpha2[i] - 'a'));
       max = Math.max(max, countReadable(words, alpha, alpha2, alpha2size, idx + 1));
-      alpha[alpha2[i] - 'a'] = false;
+      alpha &= ~(1 << alpha2[i] - 'a');
     }
 
     return max;
@@ -51,15 +40,15 @@ public class teach {
     Scanner input = new Scanner(System.in);
     // a, c, i, n, t
     char [] southPoleChs = {'a', 'c', 'i', 'n', 't'};
-    boolean [] alpha = new boolean[26];
-    boolean [] temp = new boolean[26];
+    int alpha = 0;
+    int temp = 0;
     char [] newAlpha = new char[26];
-    List<String> words = new ArrayList<String>();
+    List<Integer> words = new ArrayList<Integer>();
 
     for (int i = 0; i < 5; i++) {
       int ch = (int)(southPoleChs[i] - 'a');
-      alpha[ch] = true;
-      temp[ch] = true;
+      alpha |= (1 << ch);
+      temp |= (1 << ch);
     }
 
     int n = input.nextInt();
@@ -72,16 +61,20 @@ public class teach {
       String str = input.nextLine();
 
       String trimed = str.substring(4).substring(0, str.length() - 8);
-      words.add(trimed);
+      int word = 0;
 
       for (int i = 0; i < trimed.length(); i++) {
         char ch = trimed.charAt(i);
 
-        if (!temp[ch - 'a']) {
+        if ((temp & (1 << (ch - 'a'))) == 0) {
           newAlpha[index++] = ch;
-          temp[ch - 'a'] = true;
+          temp |= (1 << (ch - 'a'));
         }
+
+        word |= (1 << (ch - 'a'));
       }
+
+      words.add(word);
     }
 
     System.out.println(countReadable(words, alpha, newAlpha, index, 0));
