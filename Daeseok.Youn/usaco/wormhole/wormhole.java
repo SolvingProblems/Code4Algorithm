9649b61b6b67eda01a6fc9dca7d7fd992aac15e9
Daeseok Youn <daeseok.youn@gmail.com>
2017-04-04 00:35:21 +0900

    [USACO] give this problem up.
    reference:
    https://turing13.wordpress.com/2015/07/28/usaco-wormholes/
    
    But it has NOT completed yet.

--- Daeseok.Youn/usaco/wormhole/wormhole.java
+++ Daeseok.Youn/usaco/wormhole/wormhole.java
@@ -8,43 +8,83 @@ import java.util.Scanner;
 import java.util.Formatter;
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Iterator;
 
 public class wormhole {
-   public static void main (String [] args) throws IOException {
-      Scanner in = new Scanner(new File("wormhole.in"));        // file input
-      Formatter out = new Formatter(new File("wormhole.out"));  // file output
 
-      int n = Integer.parseInt(in.nextLine());
+  public static final int MAX_N = 12;
 
-      int pairs = 0;
-      Map<Integer, Integer> maps = new HashMap<Integer, Integer>();
+  public static int [] next = new int [MAX_N];
+  public static int [] pairs = new int [MAX_N];
 
-      int inCnt = n;
-      while (inCnt-- > 0) {
-        int x = Integer.parseInt(in.next());
-        int y = Integer.parseInt(in.next());
+  public static boolean is_cycle(int n) {
+    for (int s = 0; s < n; s++) {
+      int pos = s;
 
-        if (maps.containsKey(y)) {
-          maps.put(y, maps.get(y) + 1);
-        } else {
-          maps.put(y, 1);
+      for (int c = 0; c < n; c++) {
+        pos = next[pairs[pos]];
+      }
+
+      if (pos != 0)
+        return true;
+    }
+    return false;
+  }
+
+  public static int makePairs(int n) {
+    int i, total = 0;
+
+    for (i = 0; i < n; i++)
+      if (pairs[i] == 0) break;
+
+    if (i >= n - 1) {
+      if (is_cycle(n)) return 1;
+
+      return 0;
+    }
+
+    for (int j = i + 1; j < n; j++) {
+      if (pairs[j] == 0) {
+        pairs[i] = j;
+        pairs[j] = i;
+        total += makePairs(n);
+        pairs[i] = pairs[j] = 0;
+      }
+    }
+
+    return total;
+  }
+
+  public static void main (String [] args) throws IOException {
+    Scanner in = new Scanner(new File("wormhole.in"));        // file input
+    Formatter out = new Formatter(new File("wormhole.out"));  // file output
+
+    int n = Integer.parseInt(in.nextLine());
+    int [] x = new int [n];
+    int [] y = new int [n];
+
+    int inCnt = 0;
+    while (inCnt < n) {
+      x[inCnt] = Integer.parseInt(in.next());
+      y[inCnt] = Integer.parseInt(in.next());
+
+      inCnt++;
+    }
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (x[j] > x[i] && y[i] == y[j]) {
+          if (next[i] == 0 ||
+              x[j] - x[i] < x[next[i]] - x[i]) {
+            System.out.println("next[" + i + "] : " + j);
+            next[i] = j;
+          }
         }
       }
+    }
 
-      Iterator<Integer> iter = maps.keySet().iterator();
+    out.format("%d\n", makePairs(n));  // format() has the same syntax as printf()
 
-      while (iter.hasNext()) {
-        int key = iter.next();
-
-        pairs += maps.get(key)/ 2;
-      }
-
-      out.format("%d\n",pairs);  // format() has the same syntax as printf()
-
-      out.close();    // flush the output and close the output file
-      System.exit(0); // likely needed in USACO
-   }
+    out.close();    // flush the output and close the output file
+    System.exit(0); // likely needed in USACO
+  }
 }
