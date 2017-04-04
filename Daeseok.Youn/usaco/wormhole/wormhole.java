1d3457ec0a152997dc0006b237fa92f4fc10badf
Daeseok Youn <daeseok.youn@gmail.com>
2017-04-04 22:28:47 +0900

    [USACO] update wormhole
    the index of pair array could not use "0".

--- Daeseok.Youn/usaco/wormhole/wormhole.java
+++ Daeseok.Youn/usaco/wormhole/wormhole.java
@@ -13,19 +13,20 @@ public class wormhole {
 
   public static final int MAX_N = 12;
 
-  public static int [] next = new int [MAX_N];
-  public static int [] pairs = new int [MAX_N];
+  public static int [] next = new int [MAX_N + 1];
+  public static int [] pairs = new int [MAX_N + 1];
 
   public static boolean is_cycle(int n) {
-    for (int s = 0; s < n; s++) {
+    for (int s = 1; s <= n; s++) {
       int pos = s;
 
       for (int c = 0; c < n; c++) {
         pos = next[pairs[pos]];
       }
 
-      if (pos != 0)
+      if (pos != 0) {
         return true;
+      }
     }
     return false;
   }
@@ -33,16 +34,16 @@ public class wormhole {
   public static int makePairs(int n) {
     int i, total = 0;
 
-    for (i = 0; i < n; i++)
+    for (i = 1; i <= n; i++)
       if (pairs[i] == 0) break;
 
-    if (i >= n - 1) {
+    if (i > n) {
       if (is_cycle(n)) return 1;
 
       return 0;
     }
 
-    for (int j = i + 1; j < n; j++) {
+    for (int j = i + 1; j <= n; j++) {
       if (pairs[j] == 0) {
         pairs[i] = j;
         pairs[j] = i;
@@ -59,23 +60,23 @@ public class wormhole {
     Formatter out = new Formatter(new File("wormhole.out"));  // file output
 
     int n = Integer.parseInt(in.nextLine());
-    int [] x = new int [n];
-    int [] y = new int [n];
+    int [] x = new int [n + 1];
+    int [] y = new int [n + 1];
 
-    int inCnt = 0;
-    while (inCnt < n) {
+    int inCnt = 1;
+    while (inCnt <= n) {
       x[inCnt] = Integer.parseInt(in.next());
       y[inCnt] = Integer.parseInt(in.next());
 
       inCnt++;
     }
 
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n; j++) {
         if (x[j] > x[i] && y[i] == y[j]) {
           if (next[i] == 0 ||
               x[j] - x[i] < x[next[i]] - x[i]) {
-            System.out.println("next[" + i + "] : " + j);
+//            System.out.println("next[" + i + "] : " + j);
             next[i] = j;
           }
         }
